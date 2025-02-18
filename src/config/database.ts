45f556ebd46227927c935e79cd99c3b416ef4b1d
joaomatheusvillarim <joao.matheus.almeida@ccc.ufcg.edu.br>
2025-02-18 15:23:58 -0300

    corrigido problema do banco de dados

--- src/config/database.ts
+++ src/config/database.ts
@@ -6,12 +6,12 @@ dotenv.config();
 console.log(process.env.DB_USER, process.env.DB_PASS, process.env.DB_NAME);
 
 const sequelize = new Sequelize({
-  dialect: process.env.DB_DIALECT as "postgres" | "mysql",
+  dialect: process.env.DB_DIALECT as "postgres",
   host: process.env.DB_HOST,
   port: Number(process.env.DB_PORT),
-  username: process.env.DB_USER as string,
-  password: process.env.DB_PASS as string,
-  database: process.env.DB_NAME as string,
+  username: process.env.DB_USER,
+  password: process.env.DB_PASS,
+  database: process.env.DB_NAME,
   logging: false
 });
 
