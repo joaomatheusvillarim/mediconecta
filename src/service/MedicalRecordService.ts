ab8da95873ebd99e6db0c10a7aa09df639032ea9
João Matheus Villarim <joao.matheus.almeida@ccc.ufcg.edu.br>
2025-02-23 22:06:09 -0300

    service MedicalRecord

---
+++ src/service/MedicalRecordService.ts
@@ -0,0 +1,31 @@
+import { InferCreationAttributes } from 'sequelize';
+import { MedicalRecord } from '../model/MedicalRecord';
+import { MedicalRecordRepository } from '../repository/MedicalRecordRepository';
+
+const medicalRecordRepository = new MedicalRecordRepository();
+
+class MedicalRecordService {
+
+  async createMedicalRecord(data: InferCreationAttributes<MedicalRecord>): Promise<MedicalRecord> {
+    return await medicalRecordRepository.createMedicalRecord(data);
+  }
+
+  async getMedicalRecordById(id: number): Promise<MedicalRecord | null> {
+    return await medicalRecordRepository.getMedicalRecordById(id);
+  }
+
+  async getAllMedicalRecords(): Promise<MedicalRecord[]> {
+    return await medicalRecordRepository.getAllMedicalRecords();
+  }
+
+  async updateMedicalRecord(id: number, data: InferCreationAttributes<MedicalRecord>): Promise<MedicalRecord | null> {
+    return await medicalRecordRepository.updateMedicalRecord(id, data);
+  }
+
+  async deleteMedicalRecord(id: number): Promise<boolean> {
+    return await medicalRecordRepository.deleteMedicalRecord(id);
+  }
+  
+}
+
+export default new MedicalRecordService();
