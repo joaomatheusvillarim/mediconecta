5cd9a38c36c4b8101d1df780219444218c8c380c
joaomatheusvillarim <joao.matheus.almeida@ccc.ufcg.edu.br>
2025-03-17 19:41:30 -0300

    refactor AppointmentRepository

--- src/repository/AppointmentRepository.ts
+++ src/repository/AppointmentRepository.ts
@@ -1,34 +1,76 @@
-import { InferCreationAttributes } from "sequelize";
-import { Appointment } from "../model/Appointment";
+import { Appointment, AppointmentStatus } from "../model/Appointment";
 
 export class AppointmentRepository {
 
-  async createAppointment(data: InferCreationAttributes<Appointment>) {
-      return await Appointment.create(data);
+  async createAppointment(
+    clinicId: number,
+    patientId: number,
+    doctorId: number,
+    date: Date,
+    insurance: string,
+    status: AppointmentStatus
+  ) {
+    return await Appointment.create({
+      clinicId,
+      patientId,
+      doctorId,
+      date,
+      insurance,
+      status
+    });
   }
   
-  async getAppointmentById(id: number) {
-      return await Appointment.findByPk(id, { include: ['clinic'] });
+  async getAppointmentById(clinicId: number, appointmentId: number) {
+    return await Appointment.findOne({
+      where: {
+        clinicId: clinicId,
+        appointmentId: appointmentId,
+      },
+    });
   }
   
-  async getAllAppointments() {
-      return await Appointment.findAll({ include: ['clinic'] });
+  async getAllAppointments(clinicId: number) {
+    return await Appointment.findAll({
+      where: {
+        clinicId: clinicId,
+      },
+    });
   }
   
-  async updateAppointment(id: number, data: InferCreationAttributes<Appointment>) {
-      const appointment = await Appointment.findByPk(id);
-      return appointment
-        ? await appointment!.update(data)
-        : null;
-  }
-  
-  async deleteAppointment(id: number) {
-      let resp = false;
-      const appointment = await Appointment.findByPk(id);
-      if (appointment) {
-          await appointment!.destroy();
-          resp = true;
+  async updateAppointment(
+    clinicId: number,
+    appointmentId: number,
+    data: Partial<{
+      patientId: number,
+      doctorId: number,
+      date: Date,
+      insurance: string,
+      status: AppointmentStatus,
+    }>
+  ) {
+    const appointment = await Appointment.findOne({
+      where: {
+        clinicId: clinicId,
+        appointmentId: appointmentId,
       }
-      return resp;
+    });
+    return appointment
+      ? await appointment!.update(data)
+      : null;
+  }
+  
+  async deleteAppointment(clinicId: number, appointmentId: number) {
+    let resp = false;
+    const appointment = await Appointment.findOne({
+      where: {
+        clinicId: clinicId,
+        appointmentId: appointmentId,
+      }
+    });
+    if (appointment) {
+      await appointment!.destroy();
+      resp = true;
+    }
+    return resp;
   }
 }
